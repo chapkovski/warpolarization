1f87266a24e5090a6ca92f713b3918157a715253
Philipp Chapkovski <chapkovski@gmail.com>
2022-05-30 12:54:06 +0200

    upd partner positions and tests

--- settings.py
+++ settings.py
@@ -76,7 +76,7 @@ SESSION_CONFIGS = [
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=.01, participation_fee=0.00, doc=""
+    real_world_currency_per_point=.01, participation_fee=0.00, doc="", use_browser_bots=False
 )
 
 PARTICIPANT_FIELDS = []
